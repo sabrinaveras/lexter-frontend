bfb3672f3e67e6eda9f22dc44018c30a833224d7
Veras <sabrina.alves@drcash.com.br>
2020-12-14 10:20:20 -0300

    add styled to book list

--- src/pages/book-list/book-list.page.jsx
+++ src/pages/book-list/book-list.page.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 
 import * as Styled from "./book-list.styles";
 
@@ -6,12 +7,60 @@ class BookListPage extends React.Component{
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            books: [],
+            isLoading: true,
+            errors: null
+        }
+    }
+
+    getBooks(){
+
+        axios.get("https://lexter-backend.herokuapp.com/api/v1/books")
+            .then(response =>
+                response.data.body.map(book => ({
+                    title: `${book.title}`,
+                    author: `${book.author}`
+                }))
+            ).then(books => {
+                this.setState({books, isLoading: false});
+        }).catch(error => this.setState({error, isLoading: false}))
+    }
+
+    componentDidMount() {
+        this.getBooks();
     }
 
     render() {
 
+        const { isLoading, books } = this.state;
+
         return(
-            <p>List all books</p>
+            <Styled.HomePage>
+
+                {
+                    !isLoading ? (
+                        books.map((book, index) => {
+                            const { title, author} = book
+
+                            return (
+                                <Styled.Card key={index}>
+                                    <Styled.Title>
+                                        <h6>title</h6>
+                                        <h2>{title}</h2>
+                                    </Styled.Title>
+                                    <Styled.Author>
+                                        <h6>author</h6>
+                                        <h2>{author}</h2>
+                                    </Styled.Author>
+                                </Styled.Card>
+                            )
+                        })
+                    ) : (<p>Loading...</p>)
+                }
+
+            </Styled.HomePage>
         )
     }
 
--- src/pages/book-list/book-list.styles.js
+++ src/pages/book-list/book-list.styles.js
@@ -1,1 +1,79 @@
 import styled from "styled-components";
+
+export const HomePage = styled.section`
+  display: flex;
+  height: 90vh;
+  align-items: center;
+  justify-content: center;
+  font-size: 3rem;
+  flex-wrap: wrap;
+  max-width: 1440px;
+  margin: auto;
+`
+
+export const Card = styled.div`
+  width: 400px;
+  max-width: 400px;
+  height: 200px;
+  background-color: whitesmoke;
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.4);
+  transition: 0.3s;
+  border-radius: 1rem;
+
+  display: flex;
+  margin: 10px;
+  
+  &:hover{
+    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+  }
+`
+
+export const Title = styled.div`
+  width: 60%;
+  background-color: #060b26;
+  border-top-left-radius: 1rem;
+  border-bottom-left-radius: 1rem;
+  padding-top: 20px;
+  padding-left: 20px;
+  padding-right: 5px;
+  
+  h6{
+    /*opacity: 0.6;*/
+    letter-spacing: 1px;
+    text-transform: uppercase;
+    font-size: 15px;
+    color: #FFFFFF;
+    margin-bottom: 30px;
+  }
+  
+  h2{
+    letter-spacing: 1px;
+    font-size: 20px;
+    color: #FFFFFF;
+  }
+`
+
+export const Author = styled.div`
+  width: 40%;
+  /*background-color: #1a83ff;*/
+  border-top-right-radius: 1rem;
+  border-bottom-right-radius: 1rem;
+  padding-top: 20px;
+  padding-left: 20px;
+  padding-right: 5px;
+  
+  h6{
+    opacity: 0.6;
+    letter-spacing: 1px;
+    text-transform: uppercase;
+    font-size: 15px;
+    color: #060b26;
+    margin-bottom: 30px;
+  }
+  
+  h2{
+    letter-spacing: 1px;
+    font-size: 20px;
+    color: #060b26;
+  }
+`

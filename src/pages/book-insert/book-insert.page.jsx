597e1e77a2381d940d42a6eec352c54aa2af46e5
Veras <sabrina.alves@drcash.com.br>
2020-12-14 08:57:45 -0300

    add book insert page

---
+++ src/pages/book-insert/book-insert.page.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+class BookInsertPage extends React.Component{
+
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return(
+            <p>Insert book</p>
+        )
+    }
+
+}
+
+export default BookInsertPage

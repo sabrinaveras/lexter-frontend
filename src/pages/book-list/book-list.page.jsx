c152ed43258c6d5b31efed8161ce900918b304ea
Veras <sabrina.alves@drcash.com.br>
2020-12-14 08:55:49 -0300

    add book list page

---
+++ src/pages/book-list/book-list.page.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import * as Styled from "./book-list.styles";
+
+class BookListPage extends React.Component{
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+        return(
+            <p>List all books</p>
+        )
+    }
+
+}
+
+export default BookListPage

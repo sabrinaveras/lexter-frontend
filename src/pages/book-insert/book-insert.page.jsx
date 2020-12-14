ea5568c573ee1a08b4b5e2efd61648099b60803a
Veras <sabrina.alves@drcash.com.br>
2020-12-14 11:37:03 -0300

    create page insert book

--- src/pages/book-insert/book-insert.page.jsx
+++ src/pages/book-insert/book-insert.page.jsx
@@ -1,15 +1,66 @@
 import React from "react";
+import axios from "axios";
+
+import * as Styled from "./book-insert.styles";
 
 class BookInsertPage extends React.Component{
 
     constructor(props) {
         super(props);
 
+        this.state = {
+            title: "",
+            author: "",
+            creating: false,
+            errors: false
+        }
+    }
+
+    insertBook(title, author){
+
+        axios.post("https://lexter-backend.herokuapp.com/api/v1/books", {title, author})
+            .then(response => { return response.data })
+            .then( () => this.setState({creating: true}))
+            .catch(error => this.setState({error, creating: false}))
+    }
+
+    handleChange = (event) =>{
+        const name = event.target.name;
+        const value = event.target.value;
+
+        this.setState({ [name]: value })
+    }
+
+    handleSubmit = (event) =>{
+        event.preventDefault();
+
+        this.insertBook(this.state.title, this.state.author);
+
+        alert("Book created");
+
+        event.target.reset();
     }
 
     render() {
+
         return(
-            <p>Insert book</p>
+            <Styled.FromInsert onSubmit={this.handleSubmit}>
+                <Styled.CardBook>
+                    <Styled.Box backgroundBlack>
+                        <h6>title</h6>
+                        <input type={"text"} name={"title"} onChange={this.handleChange} required={true}/>
+                    </Styled.Box>
+
+                    <Styled.Box>
+                        <h6>author</h6>
+                        <input type={"text"} name={"author"} onChange={this.handleChange} required={true}/>
+                    </Styled.Box>
+
+                    <Styled.Button type="reset" reset>Clear</Styled.Button>
+                    <Styled.Button type="submit">Send</Styled.Button>
+
+                </Styled.CardBook>
+            </Styled.FromInsert>
         )
     }
 
--- src/pages/book-insert/book-insert.styles.js
+++ src/pages/book-insert/book-insert.styles.js
@@ -1,1 +1,84 @@
 import styled from "styled-components";
+
+export const FromInsert = styled.form`
+  display: flex;
+  height: 90vh;
+  align-items: center;
+  justify-content: center;
+  font-size: 3rem;
+`
+
+export const CardBook = styled.div`
+  display: block;
+  width: 500px;
+  background-color: whitesmoke;
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.4);
+  transition: 0.3s;
+  border-radius: 1rem;
+  margin: 0 10px;
+`
+
+export const Box = styled.div`
+  padding: 20px;
+  
+  h6{
+    /*opacity: 0.6;*/
+    letter-spacing: 1px;
+    text-transform: uppercase;
+    font-size: 15px;
+    color: #060b26;
+    
+    ${props => props.backgroundBlack && `
+      /*opacity: 0.6;*/
+      letter-spacing: 1px;
+      text-transform: uppercase;
+      font-size: 15px;
+      color: #FFFFFF;
+    `}
+  }
+  
+  input{
+    border-radius: 1rem;
+    border: none;
+    height: 40px;
+    text-indent: 5px;
+    width: 80%;
+    margin-bottom: 20px;
+    
+    &:focus{
+      outline: none;
+      box-shadow: none;
+    }
+  }
+  
+  ${props => props.backgroundBlack && `
+    background-color: #060b26;
+    border-top-right-radius: 1rem;
+    border-top-left-radius: 1rem;
+  `}
+`
+
+export const Button = styled.button`
+  height: 40px;
+  width: 100px;
+  border-radius: 1rem;
+  border: none;
+  background-color: #060b26;
+  color: white;
+  letter-spacing: 1px;
+  font-size: 15px;
+  margin: 0 0 20px 0;
+  cursor: pointer;
+  
+  &:focus{
+    outline: none;
+    box-shadow: none;
+  }
+  
+  ${props => props.reset &&`
+    background-color: whitesmoke;
+    border: 1px solid #060b26;
+    color: #060b26;
+    margin: 0 20px;
+  `}
+`

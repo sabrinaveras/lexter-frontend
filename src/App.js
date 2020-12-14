fbe0671500adab4d5fa5aa1e81777ecf655c6b66
Veras <sabrina.alves@drcash.com.br>
2020-12-14 09:11:45 -0300

    add route into app.js, add pages and components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import './App.css';
 
+// components
+import NavbarComponent from "./components/navbar/navbar.component";
+
+// pages
+import BookListPage from "./pages/book-list/book-list.page";
+import BookInsertPage from "./pages/book-insert/book-insert.page";
 
 function App() {
     return (
         <>
+            <Router>
+                <NavbarComponent/>
 
+                <Switch>
+                    <Route path={"/"} exact={true} component={BookListPage}/>
+                    <Route path={"/new-book"} exact={true} component={BookInsertPage}/>
+                </Switch>
+            </Router>
         </>
     );
 }

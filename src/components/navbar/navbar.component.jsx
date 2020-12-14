628361f143634a526a725542da4d0f145c1d7cd0
Veras <sabrina.alves@drcash.com.br>
2020-12-14 09:59:11 -0300

    add navbar and styles

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,13 +1,62 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
+import { Link, withRouter } from "react-router-dom";
 
-import "./navbar.styles";
+import * as IoICons from "react-icons/io";
+import * as HiIcons from "react-icons/hi";
+import { IconContext } from "react-icons";
 
-const NavbarComponent = ({}) =>{
+import { SidebarData} from "./sidebar.data";
+import { NameNavbarData } from "./name-navbar.data";
 
+import * as Styled from "./navbar.styles";
+
+const NavbarComponent = ({history}) =>{
+
+    const [sidebar, setSidebar] = useState(false);
+
+    const showSidebar = () => setSidebar(!sidebar);
 
     return (
-        <div>olá</div>
+        <>
+            <IconContext.Provider value={{color: "#FFFFFF"}}>
+
+                <Styled.Navbar>
+                    <Styled.NavBarLink to={"#"}>
+                        <HiIcons.HiMenuAlt1 onClick={showSidebar}/>
+                    </Styled.NavBarLink>
+                    {
+                        NameNavbarData.filter((item) => item.path === history.location.pathname)
+                            .map(name => (
+                                <h1 key={name.key}>{name.title}</h1>
+                            ))
+                    }
+                </Styled.Navbar>
+
+                <Styled.NavMenu active={sidebar ? "active": null}>
+                    <Styled.NavMenuItems onClick={showSidebar}>
+                        <Styled.NavbarToggle>
+                            <Styled.MenuBars to={"#"}>
+                                <IoICons.IoIosClose/>
+                            </Styled.MenuBars>
+                        </Styled.NavbarToggle>
+                        {
+                            SidebarData.map((item, index) => {
+                                return (
+                                    <Styled.NavMenuItem key={index}>
+                                        <Link to={item.path}>
+                                            {item.icon}
+                                            <span>{item.title}</span>
+                                        </Link>
+                                    </Styled.NavMenuItem>
+                                )
+                            })
+                        }
+                    </Styled.NavMenuItems>
+                </Styled.NavMenu>
+
+            </IconContext.Provider>
+        </>
     )
 }
 
-export default NavbarComponent;
+export default withRouter(NavbarComponent);
--- src/components/navbar/navbar.styles.js
+++ src/components/navbar/navbar.styles.js
@@ -1,1 +1,85 @@
 import styled from "styled-components";
+
+import { Link } from "react-router-dom";
+
+export const Navbar = styled.div`
+  background-color: #060b26;
+  height: 70px;
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  
+  h1{
+    color: #f5f5f5;
+    text-align: center;
+    margin-left: auto;
+    margin-right: auto;
+  }
+`
+
+export const NavBarLink = styled(Link)`
+  margin-left: 2rem;
+  font-size: 2rem;
+  background: none;
+`
+
+export const NavMenu = styled.nav`
+  background-color: #060b26;
+  width: 240px;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: ${props => props.active ? 0 : "-100%"};
+  transition: ${props => props.active ? "350ms" : "850ms"};
+  
+`
+
+export const NavMenuItems = styled.ul`
+  width: 100%;
+`
+
+export const NavbarToggle = styled.li`
+  background-color: #060b26;
+  width: 100%;
+  height: 80px;
+  display: flex;
+  justify-content: start;
+  align-items: center;
+`
+
+export const MenuBars = styled(Link)`
+  margin-left: 2rem;
+  font-size: 2rem;
+  background: none;
+`
+
+export const NavMenuItem = styled.li`
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  padding: 8px 0 8px 16px;
+  list-style: none;
+  height: 60px;
+  
+  a{
+    text-decoration: none;
+    color: #f5f5f5;
+    font-size: 18px;
+    width: 95%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    padding: 0 16px;
+    border-radius: 4px;
+    
+    &:hover{
+      background-color: #1a83ff;
+    }
+
+    span{
+      margin-left: 16px;
+    }
+  }
+`
